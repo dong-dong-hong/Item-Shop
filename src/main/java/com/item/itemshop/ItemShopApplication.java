b8cf474f039293a3897a4dea4dfe01e0aa0ddc6b
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-04-13 03:00:31 +0900

    QueryDSL 설정

--- src/main/java/com/item/itemshop/ItemShopApplication.java
+++ src/main/java/com/item/itemshop/ItemShopApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.item.itemshop", "com.querydsl.jpa"})
 public class ItemShopApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/item/itemshop/repository/ItemRepository.java
+++ src/main/java/com/item/itemshop/repository/ItemRepository.java
@@ -4,17 +4,18 @@ import com.item.itemshop.domain.item.Item;
 import com.item.itemshop.domain.item.QItem;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
 public class ItemRepository {
 
-    private final EntityManager em;
-   JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+    @PersistenceContext
+    private EntityManager em;
+   private JPAQueryFactory queryFactory;
 
     public void save(Item item) {
         if(item.getId() == null) {
@@ -29,7 +30,8 @@ public class ItemRepository {
     }
 
     public List<Item> findAll() {
-        return queryFactory.selectFrom(QItem.item)
+        return new JPAQueryFactory(em)
+                .selectFrom(QItem.item)
                 .fetch();
     }
 }
