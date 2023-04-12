9948c8e7809ea3d065688c0bd17ab56d1b9a8a35
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-04-13 02:37:18 +0900

    실행

--- src/main/java/com/item/itemshop/ItemShopApplication.java
+++ src/main/java/com/item/itemshop/ItemShopApplication.java
@@ -2,6 +2,7 @@ package com.item.itemshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class ItemShopApplication {
