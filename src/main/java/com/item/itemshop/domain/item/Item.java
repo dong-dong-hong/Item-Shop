d80f1722480300c9d8049397b4e8ad1dcaa8b33f
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-04-12 23:56:31 +0900

    재고 증가 및 감소(예외처리) 메서드 추가

--- src/main/java/com/item/itemshop/domain/item/Item.java
+++ src/main/java/com/item/itemshop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.item.itemshop.domain.item;
 
 import com.item.itemshop.domain.item.category.Category;
+import com.item.itemshop.exception.NotEnoughStockException;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -26,4 +27,24 @@ public abstract class Item {
 
     @OneToMany
     private List<Category> categoryItems = new ArrayList<>();
+
+    // 비즈니스 로직
+    public void addStock(int stockQuantity) {
+        this.stockQuantity += stockQuantity; // 재고 증가 및 상품 취소 시에 적용
+    }
+
+    public void removeStock(int stockQuantity) {
+        nonMinus(stockQuantity); // 재고가 음수일 경우
+        int restStock = this.stockQuantity - stockQuantity;
+        if(restStock < 0) { // 재고가 부족한 경우
+            throw new NotEnoughStockException("더 많은 재고가 필요합니다.");
+        }
+        this.stockQuantity = restStock;
+    }
+
+    private void nonMinus(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("재고는 음수가 되어서는 안됩니다.");
+        }
+    }
 }
