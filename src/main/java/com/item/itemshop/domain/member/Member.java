62758b8827b405ece0dae45a9b61c12379a71396
동동 <87763201+dong-dong-hong@users.noreply.github.com>
2023-04-13 16:48:12 +0900

    회원 수정

--- src/main/java/com/item/itemshop/domain/member/Member.java
+++ src/main/java/com/item/itemshop/domain/member/Member.java
@@ -4,16 +4,15 @@ import com.item.itemshop.domain.order.Order;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.List;
-
 
 @Entity
 @Table(name = "members")
 @Setter @Getter
 public class Member {
+
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id; // pk
@@ -23,12 +22,4 @@ public class Member {
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
